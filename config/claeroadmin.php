1733f1521a342b3196d7c97b74d96adea70faa79
Craig Nakamoto <craig.nakamoto@claero.com>
2010-11-11 14:19:26 +0000

    splitting up files

---
+++ config/claeroadmin.php
@@ -0,0 +1,33 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+return array(
+	// default options for claeroadmin controller
+	'default_list_options' => array(
+		'sort_by_column' => NULL, // claero_orm defaults to primary key
+		'sort_by_order' => NULL, // claero_orm defaults to DESC
+		'page' => 1,
+		'search' => NULL,
+	),
+	'session_key' => 'claeroadmin',
+	// default database group to use when a specific model is not loaded, or if the model does not specify a db
+	'db_group' => 'default',
+	/**
+	* Model list to be used in claeroadmin
+	* An array of model names (keys) and display names (values)
+	* Set the display name to an empty value to disable it (NULL, FALSE, etc)
+	* The first one will be used as the default or when there is an attempt to access one that doesn't exist
+	* The list will be sorted by the display name (value) before being displayed
+	*/
+	'model_list' => array(
+		// model name => display name
+		'useradmin' => 'User',
+		'authlog' => 'Auth Log',
+		'authtype' => 'Auth Type',
+		'group' => 'Group',
+		'grouppermission' => 'Group - Permission',
+		'permission' => 'Permission',
+		'usergroup' => 'User - Group',
+		'demo' => 'Demo',
+		'demosub' => 'Demo Sub',
+	),
+);
